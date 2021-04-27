b52f9567ee80ef991ebf9c76d0edc2d9a6b63c47
kojirou <kojirouhtc@gmail.com>
2021-04-28 01:33:31 +0800

    add URL extension

--- Sources/URLFileManager/URL+Extension.swift
+++ Sources/URLFileManager/URL+Extension.swift
@@ -13,4 +13,35 @@ extension URL {
     return new
   }
 
+  public func replacingMainFilename(with filename: String) -> URL {
+    var new = self
+    new.deleteLastPathComponent()
+    new.appendPathComponent(filename)
+    new.appendPathExtension(pathExtension)
+    return new
+  }
+
+  public func relativePath(to baseURL: URL, caseSensitive: Bool = false, separator: String = "/") -> String? {
+    func equal(_ l: String, _ r: String) -> Bool {
+      if _slowPath(caseSensitive) {
+        return l == r
+      } else {
+        return l.caseInsensitiveCompare(r) == .orderedSame
+      }
+    }
+
+    let baseComponents = baseURL.pathComponents
+    let longComponents = self.pathComponents
+    if baseComponents.count > longComponents.count {
+      return nil
+    }
+    for index in baseComponents.indices {
+      if !equal(baseComponents[index], longComponents[index]) {
+        return nil
+      }
+    }
+
+    return longComponents.dropFirst(baseComponents.count).joined(separator: separator)
+  }
+
 }
